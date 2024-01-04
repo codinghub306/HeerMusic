0834a1a3d93440a19c97686c6db213af8ecbe41d
Heaven <154735143+codinghub306@users.noreply.github.com>
2024-01-04 21:19:55 +0530

    Update skip.py

--- AnonXMusic/plugins/admins/skip.py
+++ AnonXMusic/plugins/admins/skip.py
@@ -9,7 +9,7 @@ from AnonXMusic.utils.database import get_loop
 from AnonXMusic.utils.decorators import AdminRightsCheck
 from AnonXMusic.utils.inline import close_markup, stream_markup
 from AnonXMusic.utils.stream.autoclear import auto_clean
-from AnonXMusic.utils.thumbnails import get_thumb
+from AnonXMusic.utils.thumbnails import gen_thumb
 from config import BANNED_USERS
 
 
